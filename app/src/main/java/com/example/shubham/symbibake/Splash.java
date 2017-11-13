ce8f19905e7e9a7981805045564ac46d709e0564
Shubham Londhe <shubham.londhe@sitpune.edu.in>
2017-11-13 18:58:51 +0530

    main activities designed

---
+++ app/src/main/java/com/example/shubham/symbibake/Splash.java
@@ -0,0 +1,30 @@
+package com.example.shubham.symbibake;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+
+    }
+
