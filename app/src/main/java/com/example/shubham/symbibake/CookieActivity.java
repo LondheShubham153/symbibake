8bea4ab045e8338b03957a108c4a9921839cc58b
Shubham Londhe <shubham.londhe@sitpune.edu.in>
2017-11-13 20:08:43 +0530

    Initial Layout
    Splash Screen
    Dummy Login
    Home Screen with Hamburger Menu
    Slide Out Menu

--- app/src/main/java/com/example/shubham/symbibake/CookieActivity.java
+++ app/src/main/java/com/example/shubham/symbibake/CookieActivity.java
@@ -7,14 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CakeActivity extends AppCompatActivity {
+public class CookieActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cake);
+        setContentView(R.layout.activity_cookie);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/shubham/symbibake/HomeActivity.java
+++ app/src/main/java/com/example/shubham/symbibake/HomeActivity.java
@@ -80,17 +80,17 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_cake) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_cookie) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_muffin) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_pastry) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_tart) {
 
         }
 
